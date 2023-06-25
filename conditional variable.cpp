5625c87e5f87f5a6d8c6d4201a0dc2baf8318bb0
Rıfat Tolga KIRAN <targrimus@gmail.com>
2023-06-26 00:01:27 +0300

    conditional variables

---
+++ conditional variable.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
+#include <atomic>
+
+
+using namespace std;
+using namespace chrono_literals;
+
+
+int main(int argc, char ** argv){
+
+    condition_variable condition;
+    mutex mtx;
+
+    bool ready = false;
+
+    thread t1([&](){
+        this_thread::sleep_for(chrono::milliseconds(2000));
+        unique_lock<mutex> lock(mtx);
+        ready = true;
+        lock.unlock();
+        condition.notify_one();
+    });
+
+    unique_lock<mutex> lock(mtx);
+
+    while (!ready)
+    {
+        condition.wait(lock);
+    }
+
+    cout << "ready " << ready << endl; 
+    
+    t1.join();
+
+return 0;
+}
